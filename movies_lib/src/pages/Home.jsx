e0cdef3c5ce8a7b0a36f4c4d9bb92645a78cf975
wharry <devwharry@gmail.com>
2024-03-21 22:07:54 -0300

    pegando o title o id e a img dos filmes pela api

--- movies_lib/src/pages/Home.jsx
+++ movies_lib/src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -20,10 +21,13 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      {
-        topMovies && topMovies.map((movie) => <p>{movie.title}</p>)
-      }
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando.....</p>}
+        {topMovies.length > 0 && 
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
